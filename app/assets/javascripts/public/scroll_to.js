be9648f68379e93f3fe55f35d449f5c52983675f
ouin85 <bouinten@yahoo.fr>
2021-05-15 01:23:12 +0200

    scroll to top button

--- app/assets/javascripts/public/scroll_to.js
+++ app/assets/javascripts/public/scroll_to.js
@@ -1,13 +1,26 @@
 $(document).ready(() => {
   let projectsBtn = document.getElementById('projects-btn');
   let watchProjectsBtn = document.getElementById('watch-projects-btn');
+  let scrollToTopBtn = document.getElementById('scroll-to-top-btn');
 
+  
   projectsBtn.addEventListener('click', (e) => scrollToProjects(e));  
-  watchProjectsBtn.addEventListener('click', (e) => scrollToProjects(e));  
+  watchProjectsBtn.addEventListener('click', (e) => scrollToProjects(e));
+  scrollToTopBtn.addEventListener('click', () => scrollTo(0));
+  window.onscroll = function() {showScrollBtn()};
   
   let scrollToProjects = (e) => {
     e.preventDefault();
     scrollTo($('#projects').offset().top);
+  }
+  
+  // When the user scrolls down 700px from the top of the document, show the button
+  function showScrollBtn() {
+    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
   }
   
   let scrollTo = (position) => {
