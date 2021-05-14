3580946a3d31c78d37ca7e67aa1c28bb8a01016b
ouin85 <bouinten@yahoo.fr>
2021-05-14 20:54:30 +0200

    add title under projects result and pokétechnos names

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
     tag_ids = params[:tag_ids]
     if not tag_ids.nil?     # AJAX request, see filter.js file
       @projects = Project.joins(:taggings).where(taggings: {tag_id: tag_ids}).distinct.order(complexity: :desc, created_at: :asc)
-      @selected_tags = tag_ids.map {Tag.find(_1).code}
+      @selected_tags = tag_ids.map { Tag.find(_1) }
     else
       @projects = Project.order(complexity: :desc, created_at: :asc)
     end
