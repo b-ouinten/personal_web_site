cd6c5499964ee162cafaa89dde98c63d711182fd
ouin85 <bouinten@yahoo.fr>
2021-05-15 15:31:07 +0200

    colorize code lines in About Me section

--- app/assets/javascripts/public/TypingEffect.js
+++ app/assets/javascripts/public/TypingEffect.js
@@ -1,15 +1,46 @@
 $(document).ready(() => {
   codeLines = [
-    "me = DeveloperFighter.new('Wazo')",
-    "me.slogan = 'Build features that benefit others'",
-    "me.forces = ['Perseverance', 'Perfection', 'Desire to learn more']",
-    "me.fight!"
+    [
+      {text: "me", color: "#92daff"}, 
+      {text: " = ", color: "white"}, 
+      {text: "DeveloperFighter", color: "#00d3b3"}, 
+      {text: ".", color: "white"},
+      {text: "new", color: "#4f93d9"},
+      {text: "(", color: "#e9bc00"},
+      {text: "'Wazo'", color: "#d58f74"},
+      {text: ")", color: "#e9bc00"}
+    ],
+    [
+      {text: "me", color: "#92daff"},
+      {text: ".", color: "white"},
+      {text: "slogan", color: "#d9e3a8"},
+      {text: " = ", color: "white"},
+      {text: "'Build features that benefit others'", color: "#d58f74"}
+    ],
+    [
+      {text: "me", color: "#92daff"},
+      {text: ".", color: "white"},
+      {text: "forces", color: "#d9e3a8"},
+      {text: " = ", color: "white"},
+      {text: "[", color: "#00c8ff"},
+      {text: "'Perseverance'", color: "#d58f74"},
+      {text: ", ", color: "white"},
+      {text: "'Perfection'", color: "#d58f74"},
+      {text: ", ", color: "white"},
+      {text: "'Desire to learn more'", color: "#d58f74"},
+      {text: "]", color: "#00c8ff"}
+    ],
+    [
+      {text: "me", color: "#92daff"},
+      {text: ".", color: "white"},
+      {text: "fight!", color: "#d9e3a8"}
+    ]
   ]
 
   typeCodeBlock(codeLines);
 
   async function typeCodeBlock(codeLines) {
-    for(i = 0, len = codeLines.length; i < len; i++ ) {
+    for(i in codeLines) {
       $('#code-lines').append(`
         <div class="typing-container">
           <span id="code-line-${i}"></span>
@@ -22,23 +53,29 @@ $(document).ready(() => {
     }
   }
 
-  async function typeCodeLine(codeLine, eleRef, delay = 100) {
-    const letters = codeLine.split("");
-    for (letter of letters) {
-      await waitForMs(delay);
-      $(eleRef).append(letter);
-    };
+  async function typeCodeLine(codeLine, codeLineEleRef, delay = 100) {
+    let codeLineIndex = parseInt(codeLineEleRef.split('').reverse().join(''));
+    for(i in codeLine) {
+      let codeLinePortionId = `code-line-portion-${codeLineIndex}${i}`;
+      $(codeLineEleRef).append(`<span id="${codeLinePortionId}"></span>`)
+      let letters = codeLine[i].text.split("");
+      for (letter of letters) {
+        await waitForMs(delay);
+        $(`#${codeLinePortionId}`).append(letter);
+        $(`#${codeLinePortionId}`).css('color', codeLine[i].color);
+      };
+    }
     
     return;
   }
   
-  async function deleteCodeLine(eleRef) {
-    const codeLine = $(eleRef).html();
+  async function deleteCodeLine(codeLineEleRef) {
+    const codeLine = $(codeLineEleRef).html();
     const letters = codeLine.split("");
     while(letters.length > 0) {
       await waitForMs(100);
       letters.pop();
-      $(eleRef).html(letters.join(""));
+      $(codeLineEleRef).html(letters.join(""));
     }
   }
   
