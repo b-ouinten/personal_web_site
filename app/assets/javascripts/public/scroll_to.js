684c61a27108bc746cbf0a8cebf222f83e87a2de
ouin85 <bouinten@yahoo.fr>
2021-05-15 08:13:20 +0200

    add about-me html element
    set up about-me btn

--- app/assets/javascripts/public/scroll_to.js
+++ app/assets/javascripts/public/scroll_to.js
@@ -1,22 +1,29 @@
 $(document).ready(() => {
   let projectsBtn = document.getElementById('projects-btn');
+  let aboutMeBtn = document.getElementById('about-me-btn');
   let watchProjectsBtn = document.getElementById('watch-projects-btn');
   let scrollToTopBtn = document.getElementById('scroll-to-top-btn');
 
-  
   projectsBtn.addEventListener('click', (e) => scrollToProjects(e));  
+  aboutMeBtn.addEventListener('click', (e) => scrollToAboutMe(e));
   watchProjectsBtn.addEventListener('click', (e) => scrollToProjects(e));
   scrollToTopBtn.addEventListener('click', () => scrollTo(0));
-  window.onscroll = function() {showScrollBtn()};
+
+  window.onscroll = () => showScrollBtn();
   
   let scrollToProjects = (e) => {
     e.preventDefault();
     scrollTo($('#projects').offset().top);
   }
+
+  let scrollToAboutMe = (e) => {
+    e.preventDefault();
+    scrollTo($('#about-me').offset().top);
+  }
   
   // When the user scrolls down 700px from the top of the document, show the button
-  function showScrollBtn() {
-    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+  let showScrollBtn = () => {
+    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
       scrollToTopBtn.style.display = "block";
     } else {
       scrollToTopBtn.style.display = "none";
