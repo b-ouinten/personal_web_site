485a32cfe89def1b1bce5434246fee4b554d9c18
ouin85 <bouinten@yahoo.fr>
2021-05-17 02:31:51 +0200

    small change

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -2,7 +2,7 @@ class PdfsController < ApplicationController
   def download_my_resume
     send_file(
       "#{Rails.root}/public/my_resume.pdf",
-      filename: "OB's resume.pdf",
+      filename: "Wazo's resume.pdf",
       type: "application/pdf"
     )
   end
